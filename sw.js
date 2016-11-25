888f90db27ac91848f488dcff48ef9c072160ec5
Martin Wragg <mdwragg@gmail.com>
2016-11-24 16:45:39 -0800

    fix paths

--- sw.js
+++ sw.js
@@ -4,21 +4,21 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('predix-ui').then(function(cache) {
      return cache.addAll([
-       '/',
-       '/contributing.html',
-       '/getting_started.html',
-       '/index.html',
-       '/landing_page.html',
-       '/px-catalog-state.html',
-       '/px-sb-list.html',
-       '/px-sb.html',
-       '/use_component_generator.html',
-       '/use_css.html',
-       '/use_seed.html',
-       '/what_is_predix_ui.html',
-       '/css/predix-ui-styles.html',
-       '/css/px-sb-list-styles.html',
-       '/css/px-sb-styles.html'
+       '/predix-ui/',
+       '/predix-ui/contributing.html',
+       '/predix-ui/getting_started.html',
+       '/predix-ui/index.html',
+       '/predix-ui/landing_page.html',
+       '/predix-ui/px-catalog-state.html',
+       '/predix-ui/px-sb-list.html',
+       '/predix-ui/px-sb.html',
+       '/predix-ui/use_component_generator.html',
+       '/predix-ui/use_css.html',
+       '/predix-ui/use_seed.html',
+       '/predix-ui/what_is_predix_ui.html',
+       '/predix-ui/css/predix-ui-styles.html',
+       '/predix-ui/css/px-sb-list-styles.html',
+       '/predix-ui/css/px-sb-styles.html'
      ]);
    })
  );
